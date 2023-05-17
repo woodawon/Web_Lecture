474c04d3114c9e2fb658dda004016558b05d00da
woodawon <woodawon0602@gmail.com>
2023-05-17 12:02:45 +0900

    js study~

--- JS_Pj/jsfile.js
+++ JS_Pj/jsfile.js
@@ -1,1 +1,20 @@
-alert("hello");
+console.log("hi hi");
+
+var age = 10;
+var str = "wow";
+var height = 158.8;
+var flag = false;
+
+// var : 전역함수 개념이고, 자료형 선언 안해도 알아서 판단해줌. => 안 좋음!
+
+let strawberry = 10;
+let cookie = "delicious";
+
+// let : 수정 가능
+
+const exnum = 100;
+const exstr = "holy";
+
+// const : 선언한 변수라면 수정 불가능
+
+// const를 기본 자료형으로 매번 사용해주고, 수정 필요하면 let 사용하기.
